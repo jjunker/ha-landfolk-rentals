96543d50a2cfeee79721295f2d5bdca9816bb23b
jjunker <jeppejunker@gmail.com>
2026-02-13 14:48:00 +0100

    Adds options flow

--- config_flow.py
+++ config_flow.py
@@ -9,7 +9,7 @@ import aiohttp
 from icalendar import Calendar
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 
@@ -56,6 +56,12 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Landfolk Rentals Calendar."""
 
     VERSION = 1
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -105,3 +111,51 @@ class CannotConnect(HomeAssistantError):
 
 class InvalidCalendar(HomeAssistantError):
     """Error to indicate the calendar data is invalid."""
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options flow for Landfolk Rentals Calendar."""
+
+    def __init__(self, config_entry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        if user_input is not None:
+            # Update the config entry with new data
+            self.hass.config_entries.async_update_entry(
+                self.config_entry,
+                data={**self.config_entry.data, **user_input}
+            )
+            # Reload the integration to apply changes
+            await self.hass.config_entries.async_reload(self.config_entry.entry_id)
+            return self.async_create_entry(title="", data={})
+
+        # Show form with current values
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_CALENDAR_URL,
+                    default=self.config_entry.data.get(CONF_CALENDAR_URL)
+                ): str,
+                vol.Optional(
+                    CONF_CHECKIN_TIME,
+                    default=self.config_entry.data.get(CONF_CHECKIN_TIME, DEFAULT_CHECKIN_TIME)
+                ): str,
+                vol.Optional(
+                    CONF_CHECKOUT_TIME,
+                    default=self.config_entry.data.get(CONF_CHECKOUT_TIME, DEFAULT_CHECKOUT_TIME)
+                ): str,
+                vol.Optional(
+                    CONF_EXCLUDE_BLOCKED,
+                    default=self.config_entry.data.get(CONF_EXCLUDE_BLOCKED, DEFAULT_EXCLUDE_BLOCKED)
+                ): bool,
+            }
+        )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=data_schema
+        )
+
